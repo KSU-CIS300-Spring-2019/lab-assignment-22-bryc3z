58a168e42fd0cee43eda3696e194ca2e6e5f57a2
bryce zimmerman <brycehzimmerman@gmail.com>
2019-03-25 15:21:55 -0500

    well that didnt get finished multiple ITries

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,41 @@
+﻿/*Author: Bryce Zimmerman
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// initialize class with ITrie Interface
+    /// </summary>
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// add method
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// contains method
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool hasEmpty = false;
+
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,65 @@
+﻿/*Author: Bryce Zimmerman
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// TrieWithOneChild class with ITrie Interface implimented
+    /// </summary>
+    public class TrieWithOneChild : ITrie
+    {
+        public TrieWithOneChild(string s, bool empty)
+        {
+            if (s == "" || (s[0]-'a' < 'a' || s[0]-'a' > 'z'))
+            {
+                throw new ArgumentException();
+            }
+            hasempty = empty;
+            childLabel = s[0];
+            //what is this doing?
+            onlyChild = new TrieWithNoChildren(s.Substring[1]);
+        }
+
+        /// <summary>
+        /// field indicating if string is empty
+        /// </summary>
+        /// 
+        private bool hasempty;
+        /// <summary>
+        /// field of ITrie with the child
+        /// </summary>
+        private ITrie onlyChild;
+
+        /// <summary>
+        /// fild of char for lable
+        /// </summary>
+        private char childLabel;
+
+        /// <summary>
+        /// adds string
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// contains method
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
